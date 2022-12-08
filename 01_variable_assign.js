68f49d8006b3378809d9f7b66c86f475bcc04dd5
toufikkhatik <toufikkhatik19@gmail.com>
2022-12-08 21:44:26 +0530

    introduction to JavaScript
    introduction to JavaScript

---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+var myFullName="Toufik Khatik";
+console.log(myFullName);
+
+var myLove="i love only JS";
+console.log(myLove);
+
+var firstName="Toufik";
+console.log(firstName);
+
+var lastName="Khatik";
+console.log(lastName);
+
+var age="38";
+console.log(age);
+
+var address="Sangola"
+console.log(address);
+
+var pin_code="413307"
+console.log(pin_code);
+
+var gender="Male";
+console.log(gender);
+
+var is_married="Married";
+console.log(is_married);
