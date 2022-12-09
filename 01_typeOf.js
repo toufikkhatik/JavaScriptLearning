d0f525b5a6e81a40a4c3c5aa710c88d90def10d1
toufikkhatik <toufikkhatik19@gmail.com>
2022-12-09 20:53:18 +0530

    typeOf
    01_typeOf asignement

---
+++ 01_typeOf.js
@@ -0,0 +1,23 @@
+var bankName; 
+bankName="Bank Name:SBI Bank";
+console.log(bankName);
+console.log(typeof bankName );
+
+var mark;
+mark="90%";
+console.log(mark);
+console.log(typeof bankName);
+
+var isWorking;
+isWorking="Is Working:true"
+console.log(isWorking);
+console.log(typeof true);
+
+var totalCount;
+totalCount="Total Count:Hundered and Seven";
+console.log(totalCount);
+
+totalCount="Total Count:107"
+totalCount=107
+console.log(totalCount);
+console.log(typeof totalCount);
